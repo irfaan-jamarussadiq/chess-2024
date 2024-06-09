ee4659b24a8ff4a273bac73b1d4cc746e2dc08d8
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2024-06-08 22:00:11 -0400

    Add methods for executing en passant, castling, and other moves

--- src/main/java/org/chess/chess/Game.java
+++ src/main/java/org/chess/chess/Game.java
@@ -24,15 +24,52 @@ public class Game {
         }
     }
 
-    private void executeMove(Move move, BoardModel board) {
-        throw new UnsupportedOperationException("Not implemented yet!");
-    }
-
     private boolean isValidMove(Move move, BoardModel board) {
         if (!move.isWithinBounds()) {
             return false;
         }
 
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    private void executeMove(Move move, BoardModel board) {
+        if (isCastlingMove(move, board)) {
+            castle(move.start(), move.end());
+        } else if (isEnPassantMove(move, board)) {
+            enPassant(move.start(), move.end());
+        } else {
+            board.movePiece(move.start(), move.end());
+        }
+    }
+
+    private void castle(Location kingStart, Location kingEnd) {
+        board.movePiece(kingStart, kingEnd);
+
+        Location rookStart, rookEnd;
+        if (kingStart.file() > kingEnd.file()) {
+            // Long castling
+            rookStart = new Location(kingStart.rank(), 1);
+            rookEnd = new Location(kingStart.rank(), 4);
+        } else {
+            // Short castling
+            rookStart = new Location(kingStart.rank(), 8);
+            rookEnd = new Location(kingStart.rank(), 6);
+        }
+
+        board.movePiece(rookStart, rookEnd);
+    }
+
+    private boolean isCastlingMove(Move move, BoardModel board) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    private void enPassant(Location pawnStart, Location pawnEnd) {
+        Location capturedPawnLocation = new Location(pawnStart.rank(), pawnEnd.file());
+        board.movePiece(capturedPawnLocation, pawnEnd);
+        board.movePiece(pawnStart, pawnEnd);
+    }
+
+    private boolean isEnPassantMove(Move move, BoardModel board) {
         throw new UnsupportedOperationException("Not implemented yet!");
     }
 
