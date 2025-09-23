ecec99583d78a4dafaac8910b8d95056bee21184
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2025-09-22 22:05:03 -0400

    Add test to BishopMoveTests class

--- src/test/java/org/chess/model/game/BishopMoveTests.java
+++ src/test/java/org/chess/model/game/BishopMoveTests.java
@@ -1,5 +1,6 @@
 package org.chess.model.game;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
@@ -32,5 +33,19 @@ public class BishopMoveTests {
         Piece piece = boardModel.pieceAt(bishopStart);
         Collection<Move> legalMoves = piece.getLegalMoves(bishopStart, boardModel);
         assertTrue(legalMoves.isEmpty());
+    }
+
+    @Test
+    public void testBishopCannotPassThroughPawn() {
+        BoardModel boardModel = new BoardModel();
+        boardModel.movePiece(new Location(2, 5), new Location(4, 5));
+        boardModel.movePiece(new Location(7, 5), new Location(5, 5));
+        boardModel.movePiece(new Location(1, 6), new Location(4, 3));
+        boardModel.movePiece(new Location(7, 4), new Location(5, 4));
+
+        Location bishopStart = new Location(4, 3);
+        Piece piece = boardModel.pieceAt(bishopStart);
+        Collection<Move> legalMoves = piece.getLegalMoves(bishopStart, boardModel);
+        assertFalse(legalMoves.contains(new Move(bishopStart, new Location(6, 5))));
     }
 }
