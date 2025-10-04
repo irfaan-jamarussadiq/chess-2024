7c26a22075e6b41b9d7570dd3c30b4dce6f44370
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2025-10-04 17:30:45 -0400

    Add legal move tests for king

---
+++ src/test/java/org/chess/model/board/KingLegalMoveTests.java
@@ -0,0 +1,54 @@
+package org.chess.model.board;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
+import org.chess.model.game.Move;
+import org.chess.model.piece.Piece;
+import org.junit.Test;
+
+public class KingLegalMoveTests {
+    @Test
+    public void testWhiteKingCannotMoveInStartingPosition() {
+        BoardModel boardModel = new BoardModel();
+        Location location = new Location(1, 5);
+        Piece piece = boardModel.pieceAt(location);
+        Collection<Move> legalMoves = piece.getLegalMoves(location, boardModel);
+        assertEquals(0, legalMoves.size());
+    }
+
+    @Test
+    public void testBlackKingCannotMoveInStartingPosition() {
+        BoardModel boardModel = new BoardModel();
+        Location location = new Location(8, 5);
+        Piece piece = boardModel.pieceAt(location);
+        Collection<Move> legalMoves = piece.getLegalMoves(location, boardModel);
+        assertEquals(0, legalMoves.size());
+    }
+
+    @Test
+    public void testWhiteKingCanOnlyMoveToRankOneFileFive() {
+        BoardModel boardModel = new BoardModel();
+        Location location = new Location(1, 5);
+        boardModel.movePiece(new Location(2, 5), new Location(4, 5));
+        boardModel.movePiece(new Location(7, 5), new Location(5, 5));
+        Piece piece = boardModel.pieceAt(location);
+        Collection<Move> legalMoves = piece.getLegalMoves(location, boardModel);
+        assertEquals(1, legalMoves.size());
+        assertTrue(legalMoves.contains(new Move(location, new Location(2, 5))));
+    }
+
+    @Test
+    public void testBlackKingCanOnlyMoveToRankEightFileFive() {
+        BoardModel boardModel = new BoardModel();
+        Location location = new Location(8, 5);
+        boardModel.movePiece(new Location(2, 5), new Location(4, 5));
+        boardModel.movePiece(new Location(7, 5), new Location(5, 5));
+        Piece piece = boardModel.pieceAt(location);
+        Collection<Move> legalMoves = piece.getLegalMoves(location, boardModel);
+        assertEquals(1, legalMoves.size());
+        assertTrue(legalMoves.contains(new Move(location, new Location(7, 5))));
+    }
+}
